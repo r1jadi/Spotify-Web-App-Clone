0b518a9775a01615f4e5bbe83b27cafadd97be8a
Rron Buza <151071846+rronbuza@users.noreply.github.com>
2023-12-02 13:43:21 +0100

    log in validation 100%

---
+++ loginscript.js
@@ -0,0 +1,34 @@
+function validateLogIn(){
+
+    let email = document.getElementById('usremail').value;
+    let password = document.getElementById('psw').value;
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        alert('Please enter a valid email adress');
+        return false;
+    }
+
+    let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    if(!passwordRegex.test(password)){
+        alert('Please enter a valid password');
+        return false;
+    }
+}
+
+/*function setRememberMeCookie() {
+    document.cookie = "rememberMe=true; expires=Sun, 31 Dec 2030 12:00:00 UTC; path=/";
+}
+
+function checkRememberMeCookie() {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf("rememberMe=true") === 0) {
+            // Log in the user automatically
+            // Your login logic here
+            return true;
+        }
+    }
+    return false;
+}*/ 
