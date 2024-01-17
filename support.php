f8f5c23c0bdefeb1b97518b0673ec0d819c371c8
Rijad Halili <rijadhalili.yt@gmail.com>
2024-01-17 23:01:17 +0100

    update

--- support.php
+++ support.php
@@ -4,63 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Support - Spotify</title>
-    <link rel="stylesheet" href="aboutus.css">
+    <link rel="stylesheet" href="supportcss.css">
 
 
 </head>
 <body>
-
-    <nav>
-        <div class="navaboutus">
-            <a href="homepage.php"><img src="Images/headerlogo.png" alt="Header Logo" class="navheaderlogo"></a>
+    
+<header>
+        <div class="navhomepage">
+            <a href="loginpage.php"><img src="Images/headerlogo.png" alt="Header Logo" class="navheaderlogo"></a>
             <div class="navlist">
-                <a href="homepage.php" class="navanchor">Home page</a>
-                <a href="loginpage.php" class="navanchor">Log in</a>
-                <a href="signuppage.php" class="navanchor">Sign up</a>
+                <a href="homepage.php" class="navanchor">Home</a>
+                <a href="#" class="navanchor">Playlists</a>
+                <a href="aboutus.php" class="navanchor">About</a>
+                <a href="#" class="navanchor">Contact</a>
             </div>
         </div>
-    </nav>
-    
-    <main class="firstmain">
+    </header>
 
-    <div>
-        <a href="help.php"><h2>How can we help you?</h2></a>
-        <a href=""><h3>Log-in</h3></a><p>for faster help</p>
-    </div>
-
+    <main class="main1">
+        <div class="help">
+        <p>Spotify Support</p>    
+        <h2>How can we help you?</h2>
+        <p><a href="" class="anchorcl">Log-in</a> to get faster help</p>
+        </div>        
     </main>
 
-    
-    <footer>
-    
-        <div class="kolona1">
-            <h3>Company</h3>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">For the record</a></li>
+    <main class="main2">
+        <div class="lista">
+            <a href="" class="anchorcl1"><li class="lista1">Can't remember login details</li></a>
+            <a href="" class="anchorcl1"><li class="lista1">Payment methods</li></a>
+            <a href="" class="anchorcl1"><li class="lista1">Price updates</li></a>
+            <a href="" class="anchorcl1"><li class="lista1">Spotify's 2023 Wrapped FAQ</li></a>
         </div>
-
-        <div class="kolona2">
-            <h3>Communities</h3>
-            <li><a href="#">For artists</a></li>
-            <li><a href="#">Developers</a></li>
-            <li><a href="#">Advertising</a></li>
-            <li><a href="#">Investors</a></li>
-            <li><a href="#">Vendors</a></li>
-        </div>
-
-        <div class="kolona3">
-            <h3>Useful links</h3>
-            <li><a href="#">Support</a></li>
-            <li><a href="#">Free mobile app</a></li>
-        </div>
-
-   </footer>
+    </main>
 
    <div class="footerbottom">
             <p>© 2023 Spotify AB</p>
         </div>
 
-        <?php  ?>
-</body>
 </html>
