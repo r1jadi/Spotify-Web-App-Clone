fc0ef3343f8d4e370d59ac06308d6bae756815f5
Rijad Halili <rijadhalili.yt@gmail.com>
2024-01-18 20:34:59 +0100

    update

--- terms.php
+++ terms.php
@@ -15,14 +15,43 @@
         </div>
     </header>
 
-    <main>
+    <main class="main1">
         
-    <div class="maindiv">
+    <div class="maindiv div1">
         <h2>1. Introduction</h2>
         <p>Please read carefully the following Terms of Use (these “Terms”) which govern your use of Spotify's personalized services for streaming music and other <br>
          content, including all of our websites and software applications incorporating or link these Terms (collectively, the “ Spotify Service ”) and any music, videos, <br>
          podcasts or other materials made available through the Spotify Service (“ Content(s) ”).</p>
+
+        <h2>2. Changes to the agreement</h2>
+        <p>
+        From time to time, we may make changes to the Agreement for legitimate reasons, such as to improve existing functions or features or <br>
+         to add new functions or features to the Spotify Services, to reflect scientific and technical advances, or to make reasonable technical adjustments to ensure the functionality or security <br> 
+         of the Spotify Services. to perform services and for legal or regulatory reasons. If we make changes to the Agreement that may affect the ongoing contractual <br>
+          relationship between you and us, we will provide you with advance notice appropriate to the circumstances, for example by displaying a prominent notice, <br>
+           sending you an email, or by asking for your consent within the Spotify Services. This notice will include information about the proposed changes and your right <br>
+            to reject those changes, where to send the rejection and the consequences if you do not reject. <br>
+            The changes will be deemed accepted if you do not reject them within 30 days. Using this procedure, we will not make any changes that materially affect the contractual balance between the Spotify Services <br>
+            and the consideration you may provide for them. If you no longer wish to use the Spotify Services under the new version of the Agreement, you may terminate <br>
+            your account by contacting us. If you received a trial subscription or a paid subscription through a third party, you should, if possible, cancel the applicable paid <br>
+            subscription directly through that third party.
+        </p>
+
+        <h2>3. The Spotify services offered </h2>
+        <p>
+        We offer numerous options on Spotify services. Certain options of the Spotify Services are offered free of charge, while other options require payment before <br>
+         they can be used (the "Paid Subscriptions"). We may also offer special promotions, memberships or Spotify services, <br>
+          including offers of third party products and services.
+        </p>
+
+        <h2>4. Billing</h2>
+        <p>
+        You may purchase a Paid Subscription directly from Spotify or through a third party (1) by paying a subscription fee in advance each month or at such other <br>
+         period as we will notify you prior to your purchase, or (2) by making an upfront payment You will receive access to the Spotify Services for a specific period of <br>
+          time (“Prepaid Period”).
+        </p>
     </div>
     </main>
+    
 </body>
 </html>
