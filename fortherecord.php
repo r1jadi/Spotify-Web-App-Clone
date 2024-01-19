f4a62ac42279e178c9b806971e4cc444ac320480
Rijad Halili <rijadhalili.yt@gmail.com>
2024-01-19 21:31:38 +0100

    front end final

---
+++ fortherecord.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For the record - Spotify</title>
+    <link rel="stylesheet" href="fortherecord.css">
+
+</head>
+
+<body>
+
+    <!-- Fillimi i kodit per header -->
+    <header>
+        <div class="headerlogin">
+            <a href="homepage.php"><img class="hlogin" src="Images/headerlogo.png" alt="Header Logo" ></a>
+        </div>
+    </header> <!-- Fundi i kodit per header --> 
+    
+    <main>
+
+    <h4>MUSICAL TIME CAPSULE</h4>
+
+    <h2>
+    It’s Time To Unlock Your Playlist In a Bottle From 2023 (and Make a New One for Next Year)
+    </h2>
+
+    <p>JANUARY 4, 2024</p>
+
+    <p>
+    Quick pulse check: What’s your mood, your inspiration, and your mindset as you kick off <br> 
+    2024? Does it match your feelings from a year ago? 
+    <br>
+    <br>
+    
+    Last year we introduced “Playlist in a Bottle,” giving listeners a chance to capture the songs <br>
+     that resonated with them at that moment in time and seal it away for one year. The <br> 
+     interactive in-app experience is the perfect time capsule to remember and reflect on the <br> 
+     music you loved, and how your taste may have evolved over the past 365 days. If you’re one <br>
+      of the millions of Spotify listeners who participated then, today is the day to unlock your <br>
+       “Playlist in a Bottle” from last year. </p>
+
+       <h2>
+       Here’s how
+       </h2>
+
+       <p>
+       Visit spotify.com/playlistinabottle on your mobile device. <br>
+
+    Choose “Claim Your Playlist” to see which songs were sealed in your musical time capsule. <br>
+
+    Save and listen to your complete 2023 Playlist in a Bottle. You only have until January 31!
+    <br>
+    <br>
+
+    <span>
+    Now it’s time to create a new Playlist in a Bottle to open in 2025. This experience is open to <br>
+     all Spotify listeners—whether you made one last year or this is your first time checking out <br>
+      the experience. This time around, you’ll answer questions like “what song brings out my  <br>
+      alter-ego” and “what song describes my relationship status” to curate your playlist. You can <br>
+      also select where you want to store your playlist from a series of objects, and write a note to <br>
+      your future self.</span>
+       </p>
+    </main>
+
+<footer>
+    <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+   </footer>
+
+   <?php  ?>
+</body>
+</html>
