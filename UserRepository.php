52e72fc241fc9080d6706e128fd44405118fa331
Rron Buza <realestopp0@gmail.com>
2024-01-24 15:01:53 +0100

    update

---
+++ UserRepository.php
@@ -0,0 +1,83 @@
+<?php 
+include_once 'DatabaseConnection.php';
+
+class UserRepository{
+    private $connection;
+
+    function __construct(){
+        $conn = new DatabaseConenction;
+        $this->connection = $conn->startConnection();
+    }
+
+
+    function insertUser($user){
+
+        $conn = $this->connection;
+
+        $id = $user->getId();
+        $email = $user->getEmail();
+        $password = $user->getPassword();
+        $name = $user->getName();
+
+        $sql = "INSERT INTO user (id,email,password,name) VALUES (?,?,?,?)";
+
+        $statement = $conn->prepare($sql);
+
+        $statement->execute([$id,$email,$password,$name]);
+
+        echo "<script> alert('User has been inserted successfuly!'); </script>";
+
+    }
+
+    function getAllUsers(){
+        $conn = $this->connection;
+
+        $sql = "SELECT * FROM user";
+
+        $statement = $conn->query($sql);
+        $users = $statement->fetchAll();
+
+        return $users;
+    }
+
+    function getUserById($id){
+        $conn = $this->connection;
+
+        $sql = "SELECT * FROM user WHERE id='$id'";
+
+        $statement = $conn->query($sql);
+        $user = $statement->fetch();
+
+        return $user;
+    }
+
+    function updateUser($id,$email,$password,$name){
+         $conn = $this->connection;
+
+         $sql = "UPDATE user SET name=?, email=?, password=? WHERE id=?";
+
+         $statement = $conn->prepare($sql);
+
+         $statement->execute([$name,$email,$password,$id]);
+
+         echo "<script>alert('update was successful'); </script>";
+    } 
+
+    function deleteUser($id){
+        $conn = $this->connection;
+
+        $sql = "DELETE FROM user WHERE id=?";
+
+        $statement = $conn->prepare($sql);
+
+        $statement->execute([$id]);
+
+        echo "<script>alert('delete was successful'); </script>";
+   } 
+}
+
+//  $userRepo = new UserRepository;
+
+//  $userRepo->updateUser('1111','SSS','SSS','SSS','SSS','SSS');
+
+?>
