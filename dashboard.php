e0625cb4d434f8224d81e3c0151f63d0a0b8a137
Rron Buza <realestopp0@gmail.com>
2024-02-01 15:34:21 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <!--link rel="stylesheet" href="dashboardstyle.css"-->
 
     <style>
-        body {
+body {
     font-family: 'Arial', sans-serif;
     margin: 0;
     padding: 0;
