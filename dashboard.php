1f8365983b5bd10a3f290b303895683da1c5fe01
Rron Buza <realestopp0@gmail.com>
2024-02-01 15:45:06 +0100

    dashboard update

--- dashboard.php
+++ dashboard.php
@@ -3,88 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboardstyle.css">
     <title>Admin Dashboard</title>
-
-    <style>
-body {
-    font-family: 'Arial', sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: #f4f4f4;
-}
-
-header {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
-}
-
-nav {
-    background-color: #555;
-    color: #fff;
-    padding: 10px;
-}
-
-nav ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-}
-
-nav ul li {
-    margin-right: 20px;
-}
-
-nav a {
-    text-decoration: none;
-    color: #fff;
-    font-weight: bold;
-    font-size: 16px;
-}
-
-section {
-    padding: 20px;
-}
-
-table{
-    display: flex;
-    justify-content: center ;
-}
-
-footer {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-}
-
-
-@media screen and (max-width: 768px) {
-    nav ul {
-        flex-direction: column;
-        align-items: center;
-    }
-
-    nav ul li {
-        margin: 10px 0;
-    }
-}
-
-@media screen and (max-width: 600px) {
-    header h1 {
-        font-size: 1.5em;
-    }
-
-    nav a {
-        font-size: 14px;
-    }
-}
-    </style>
 </head>
 <body>
 
@@ -94,13 +14,20 @@ footer {
 
     <nav>
         <ul>
-            <li><a href="InsertMusic.php">Insert Music</a></li>
             <li><a href="Read.php">Read User</a></li>
+            <li><a href="Edit.php">Edit User</a></li>
+            <li><a href="Delete.php">Delete User</a></li>
+
+            <li><a href="InsertMusic.php">Insert Music</a></li>
             <li><a href="ReadMusic.php">Read Music</a></li>
             <li><a href="EditMusic.php">Edit Music</a></li>
-            <li><a href="Edit.php">Edit User</a></li>
             <li><a href="DeleteMusic.php">Delete Music</a></li>
-            <li><a href="Delete.php">Delete User</a></li>
+            
+            <li><a href="InsertPlaylist.php">Insert Playlist</a></li>
+            <li><a href="ReadPlaylists.php">Read Playlist</a></li>
+            <li><a href="UpdatePlaylist.php">Update Playlist</a></li>
+            <li><a href="DeletePlaylist.php">Delete Playlist</a></li>
+
             <li><a href="logout.php">Logout</a></li>
         </ul>
     </nav>
