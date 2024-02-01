1f06814c0b0670ddb7fada39ed651cc21870374c
Rron Buza <realestopp0@gmail.com>
2024-02-01 15:55:33 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -8,33 +8,39 @@
 </head>
 <body>
 
-    <header>
-        <h1>Welcome, Admin!</h1>
-    </header>
+<header>
+    <h1>Welcome, Admin!</h1>
+</header>
 
-    <nav>
-        <ul>
-            <li><a href="Read.php">Read User</a></li>
-            <li><a href="Edit.php">Edit User</a></li>
-            <li><a href="Delete.php">Delete User</a></li>
+<nav>
+    <ul>
+        <li><a href="signuppage.php">Sign up</a></li>
+        <li><a href="Read.php">Read User</a></li>
+        <li><a href="Edit.php">Edit User</a></li>
+        <li><a href="Delete.php">Delete User</a></li>
 
-            <li><a href="InsertMusic.php">Insert Music</a></li>
-            <li><a href="ReadMusic.php">Read Music</a></li>
-            <li><a href="EditMusic.php">Edit Music</a></li>
-            <li><a href="DeleteMusic.php">Delete Music</a></li>
-            
-            <li><a href="InsertPlaylist.php">Insert Playlist</a></li>
-            <li><a href="ReadPlaylists.php">Read Playlist</a></li>
-            <li><a href="UpdatePlaylist.php">Update Playlist</a></li>
-            <li><a href="DeletePlaylist.php">Delete Playlist</a></li>
+        <li><a href="InsertMusic.php">Insert Music</a></li>
+        <li><a href="ReadMusic.php">Read Music</a></li>
+        <li><a href="EditMusic.php">Edit Music</a></li>
+        <li><a href="DeleteMusic.php">Delete Music</a></li>
 
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-    </nav>
-    
-    <footer>
-        <p>&copy; 2024 Admin Dashboard</p>
-    </footer>
+        <li><a href="InsertPlaylist.php">Insert Playlist</a></li>
+        <li><a href="ReadPlaylists.php">Read Playlist</a></li>
+        <li><a href="UpdatePlaylist.php">Update Playlist</a></li>
+        <li><a href="DeletePlaylist.php">Delete Playlist</a></li>
+
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
+
+<section>
+    <h2>This is the Admin Dashboard Page!</h2>
+    <p>Be careful and do not change any sensitive information!</p>
+</section>
+
+<footer>
+    <p>&copy; 2024 Admin @ Spotify</p>
+</footer>
 
 </body>
 </html>
